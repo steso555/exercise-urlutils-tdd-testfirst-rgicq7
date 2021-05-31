ab0f1c34bb4d3f5746ebc756a34079d79894a985
steso555 <steso555@gmail.com>
2021-05-31 22:22:39 +0200

    Return object of parameters from the passed query string

--- src/urlutils.ts
+++ src/urlutils.ts
@@ -8,11 +8,13 @@ export function pairs(object) {
 
   return arr;
 }
+
 export function params(obj) {
   // Return url params string from input object
   // { "foo": 42, "bar": 82 } => "foo=42&bar=82"
   // { 'foo':['x','y'] } => "foo=x&foo=y"
-
+  // Use pairs(), you may use Array.prototype.map()
+  // Avoid for/while loops here
   function keyValueString(keyAndValue) {
     let key = keyAndValue[0];
     //console.log('key= ', key);
@@ -22,24 +24,46 @@ export function params(obj) {
 
     if (Array.isArray(value)) {
       let urlParams = value
-      .filter(el => el !== null  &&  !(isNaN(el) && typeof el == 'number') && el !== undefined)
+        .filter(
+          el =>
+            el !== null &&
+            !(isNaN(el) && typeof el == 'number') &&
+            el !== undefined
+        )
         .map(encodedValue => key + '=' + encodedValue)
         .join('&');
-      //console.log(urlParams);
-
       return urlParams;
     }
-    //console.log(`${encodedKey}=${encodedValue}`);
+    //console.log(`${decodeKey}=${decodeValue}`);
     return `${encodedKey}=${encodedValue}`;
   }
 
   let urlParams = Object.entries(obj)
-    .filter(([key,value]) => value !== null  &&  !(isNaN(Number(value)) && typeof value == 'number') && value !== undefined)
+    .filter(([key, value]) => value !== null && !(isNaN(Number(value)) && typeof value == 'number') && value !== undefined)
     .map(keyValueString)
     .join('&');
   //console.log(urlParams);
   return urlParams;
 }
 
-// Use pairs(), you may use Array.prototype.map()
-// Avoid for/while loops here
+export function parseParams(str) {
+  //console.log(str);
+  function splitPaar(keyValuePair) {
+    //console.log("2",keyValuePair)
+    let splitkeyValuePairsArr = keyValuePair.split('=');
+    //console.log('split[]:', splitkeyValuePairsArr);
+    let decodeUri = splitkeyValuePairsArr.map(decodeURIComponent);
+    //console.log("Test: ", decodeUri)
+    return decodeUri;
+  }
+
+  //1. Split(str) in paairs of keys and values
+  let keyValuePairs = str.split('&');
+  //console.log('keyValue: ', keyValuePairs);
+  let keyValuePairSplit = keyValuePairs.map(splitPaar);
+  console.log('1: ', keyValuePairSplit);
+
+  let obj = Object.fromEntries(keyValuePairSplit);
+  console.log('final: ', obj);
+  return obj;
+}
