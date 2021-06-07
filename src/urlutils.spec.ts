87e6343f94f9819450aba82aad655d9d1b9d6ddd
steso555 <steso555@gmail.com>
2021-06-07 17:18:20 +0200

    Refactoring and Test divided

--- src/urlutils.spec.ts
+++ src/urlutils.spec.ts
@@ -2,27 +2,25 @@ import { pairs, params, parseParams } from './urlutils';
 
 describe('urlutils', () => {
   describe('pairs', () => {
+    
     it('should return the object keys as pair arrays', () => {
-      // Expect the result of pairs() to be equal to a value
-      expect(pairs({ foo: 'bar' })).toEqual([['foo', 'bar']]);
+      expect(pairs({ foo: 'bar' })).toEqual(
+        [['foo', 'bar']]
+        );
     });
-    // More than a key
+
     it('should return the object with more than one key as pair arrays', () => {
-      expect(pairs({ name: 'Alex', age: 35 })).toEqual([
-        ['name', 'Alex'],
-        ['age', 35]
-      ]);
+      expect(pairs({ name: 'Alex', age: 35 })).toEqual(
+        [['name', 'Alex'], ['age', 35]]
+        );
     });
-    // Write more sensible tests.
-    // For example, ensure bad input values produce sensible
-    // results.
+
     it('bad input values produce sensible results', () => {
       expect(pairs({ name: '', age: 35 })).toEqual([['name', ''], ['age', 35]]);
     });
   });
 
   describe('params', () => {
-    // Write sensible tests.
     it('should return url params string from input object', () => {
       expect(params({ foo: 42, bar: 82, loop: '' })).toEqual(
         'foo=42&bar=82&loop='
@@ -36,8 +34,7 @@ describe('urlutils', () => {
     it('should handled empty values', () => {
       expect(params({})).toEqual('');
     });
-    // Ensure the values are URL encoded and different types
-    // are handled correctly
+
     it('should handled correctly different types', () => {
       expect(params({ foo: ['x', 'y'] })).toEqual('foo=x&foo=y');
     });
@@ -52,18 +49,23 @@ describe('urlutils', () => {
   });
 
   describe('parseParams', () => {
-    // Return object of parameters from the passed query string
-    // IN: "foo=42&b%2Cr=B%C3%A4r&baz=47&baz=94&maz="
-    // OUT: { "foo": "42", "b,r": "Bär", baz: ["47", "94"], maz: "" }
-    // URL-decode keys and values
+
     it('should return object of parameters from the passed query string', () => {
-      expect(parseParams('foo=42&b%3Dr=B%C3%A4r&baz=66&baz=54&maz=')).toEqual(
-        { "foo": "42", "b=r": "Bär", baz: ["66", "54"], maz: "" }
-      );
+      expect(parseParams('foo=42&baz=66&baz=54&maz=')).toEqual(
+        { foo: '42', baz: ['66', '54'], maz: '' }
+        );
     });
+
     it('should return object with array of all repeated keys', () => {
       expect(parseParams('baz=47&baz=94&baz=108')).toEqual(
-        { baz: ['47', '94', '108'], });
+        { baz: ['47', '94', '108'] }
+        );
+    });
+
+    it('should return object of parameters from the passed EncodeURI query string', () => {
+      expect(parseParams('b%3Dr=B%C3%A4r')).toEqual(
+        { 'b=r': 'Bär' }
+        );
     });
   });
 });
